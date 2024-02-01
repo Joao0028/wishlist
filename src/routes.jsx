1f45da82aec67e70a541b5fba6bb4614f5e287b2
Joao0028 <joaopaulonascimento720@gmail.com>
2024-02-01 12:38:09 -0300

    Configurando paleta de cores do Tailwind e Sass

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,16 @@
+import { lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/PageHome"))
+const Lista = lazy(() => import("./pages/PageList"))
+
 export default function AppRoutes(){
     return <main>
-        Hello World
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/ListaItens" element={<Lista />}/>
+            </Routes>
+        </BrowserRouter>
     </main>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cores: {
+          $roxoClaroNetshoes: "#5a2d82",
+          $roxoEscuroNetshoes: "#370f53",
+          $branco: "#ffffff",
+          $preto: "#000000",
+          $cinzaEscuro: "#333333",
+          $cinzaClaro: "#4d4d4d",
+          $laranja: "#ff8b51",
+          $azulEscuro: "#0091ea",
+          $azulClaro: "#37aaf3",
+          $verdeClaroBackground: "#07c278cc",
+          $verdeClaroTexto: "#10ba78",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
